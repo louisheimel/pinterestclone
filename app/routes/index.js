50bb5eaf99a4870e82f80b0d08e1eef4b3837ac7
Unknown Name <louis.heimel@mines.sdsmt.edu>
2017-03-04 18:59:52 +0000

    implement login/logout

--- app/routes/index.js
+++ app/routes/index.js
@@ -32,7 +32,7 @@ module.exports = function (app, passport) {
 	app.route('/logout')
 		.get(function (req, res) {
 			req.logout();
-			res.redirect('/login');
+			res.redirect('/');
 		});
 
 	app.route('/profile')
