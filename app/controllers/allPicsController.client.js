b42668b234e3ea6789665655da359608a6edd576
Unknown Name <louis.heimel@mines.sdsmt.edu>
2017-03-06 15:51:54 +0000

    render all pics to main page

--- app/controllers/allPicsController.client.js
+++ app/controllers/allPicsController.client.js
@@ -3,6 +3,14 @@
 (function() {
     ajaxFunctions.ready(
         ajaxFunctions.ajaxRequest('GET', '/all_pics', function(data) {
+        var grid = document.querySelector('.masonry-grid');
+        JSON.parse(data).forEach((datum) => {
+            var img = document.createElement('img'),
+                div = document.createElement('div');
+            img.setAttribute('src', datum.url);
+            div.appendChild(img);
+            grid.appendChild(div);
+        })
         console.log(data);
     }));
 })();
--- app/controllers/myPicsController.client.js
+++ app/controllers/myPicsController.client.js
@@ -1,8 +1,16 @@
 'use strict';
 
 (function() {
+    var body = document.getElementsByTagName('body')[0];
+    
     ajaxFunctions.ready(
         ajaxFunctions.ajaxRequest('GET', '/get_my_pics', function(data) {
+            console.log(data)
+        JSON.parse(data).forEach((pic) => {
+            var newPic = document.createElement('img');
+            img.setAttribute('href', pic.url);
+            body.appendChild(img)
+        })
         console.log(data);
     }));
 })();
