9d26fc757ed866c5ac47bacb149dd4108ac50b5f
Unknown Name <louis.heimel@mines.sdsmt.edu>
2017-03-13 17:24:42 +0000

    some

--- app/routes/index.js
+++ app/routes/index.js
@@ -63,7 +63,7 @@ module.exports = function (app, passport) {
 	app.route('/get_my_pics')
 		.get(isLoggedIn, function(req, res, next) {
 			Pic.find({}).then(function(pics) {
-				res.end(JSON.parse(pics));
+				res.json({pics: pics, id: req.user._id});
 			})
 			
 		})
